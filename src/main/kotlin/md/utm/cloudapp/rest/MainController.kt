b4139f805ae71a635310c76cd341047fc35a07aa
Denis Trifan <trifan.denis1999@gmail.com>
2024-04-17 16:23:40 +0300

    Updated MainController

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -2,12 +2,15 @@ package md.utm.cloudapp.rest
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Random
 
 @RestController
 class MainController {
 
     @GetMapping("/")
     fun main(): String {
-        return "Hello World!"
+        val random = Random()
+        val randomNumber = random.nextInt()
+        return "Hello World! seed: $randomNumber"
     }
 }
