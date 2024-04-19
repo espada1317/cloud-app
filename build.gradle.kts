373b41983c3645353876b02bd6662cd99bf4db2a
Denis Trifan <trifan.denis1999@gmail.com>
2024-04-19 12:08:26 +0300

    Applied actuator for prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
