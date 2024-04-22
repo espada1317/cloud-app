2c4151c10acf6ebd0b3f14a95955d105dd97921c
Denis Trifan <trifan.denis1999@gmail.com>
2024-04-22 10:57:05 +0300

    Added code to display running container

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -4,19 +4,29 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.Random
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 
 @RestController
 class MainController {
 
     private val logger = KotlinLogging.logger {}
 
+    @Autowired
+    private lateinit var env: Environment
+
     @GetMapping("/")
     fun main(): String {
+
+        val containerName = env.getProperty("HOSTNAME")
+
+        logger.info { "Container Name: $containerName" }
+
         val random = Random()
         val randomNumber = random.nextInt()
 
         logger.info { "Generated random number: $randomNumber" }
 
-        return "Hello World! seed: $randomNumber"
+        return "Hello World! with random number: $randomNumber\nFrom container: $containerName"
     }
 }
